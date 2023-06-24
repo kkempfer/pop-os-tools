6888f2376982dd16e644044026bbb0875d7e6aac
Kévin Kempfer <kevin.kempfer@hotmail.fr>
2023-06-23 22:33:35 -0400

    steam integrated GPU

--- setup.sh
+++ setup.sh
@@ -90,6 +90,8 @@ apt install playonlinux
 # https://support.system76.com/articles/linux-gaming/
 
 apt install steam
+# https://askubuntu.com/questions/1378348/steam-launches-using-terminal-but-not-with-launcher-icon-ubuntu-21-10-nvidia
+sed -i 's/PrefersNonDefaultGPU=true/PrefersNonDefaultGPU=false/g' /usr/share/applications/steam.desktop
 # Enable proton support in Steam > Settings > Compatibility
 # Enable shader pre-caching in Downloads > Shader pre-caching
 # TODO https://support.system76.com/articles/linux-gaming/#use-protonup-to-manage-custom-proton-versions
