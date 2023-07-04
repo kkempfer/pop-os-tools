58902859f56c598c96179ea4e5de86320780c524
Kévin Kempfer <kevin.kempfer@hotmail.fr>
2023-07-04 11:02:30 -0400

    Added Minecraft

--- setup.sh
+++ setup.sh
@@ -125,6 +125,7 @@ ln -s ~/.minecraft ~/Games/minecraft
 mkdir -p ~/Downloads/minecraft/1.20.1/
 pushd ~/Downloads/minecraft/1.20.1/
 
+# https://optifine.net/downloads
 chmod a+x preview_OptiFine_1.20.1_HD_U_I5_pre6.jar
 java -jar preview_OptiFine_1.20.1_HD_U_I5_pre6.jar
 
@@ -134,6 +135,19 @@ java -jar preview_OptiFine_1.20.1_HD_U_I5_pre6.jar
 # https://www.curseforge.com/minecraft/customization/kappa-shader-by-rre36
 # https://www.curseforge.com/minecraft/customization/complementary-shaders
 # https://continuum.graphics/continuum-shaders
+
+mkdir fabric
+pushd fabric
+
+# https://fabricmc.net/
+wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.2/fabric-installer-0.11.2.jar
+chmod a+x fabric-installer-0.11.2.jar
+java -jar fabric-installer-0.11.2.jar
+
+# https://www.curseforge.com/minecraft/mc-mods/fabric-api
+# https://www.curseforge.com/minecraft/mc-mods/falling-tree
+
+popd
 
 popd
 
