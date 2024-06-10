e399f9da1abc6bc72e0d05855aa17953be95e996
Kévin Kempfer <kevin.kempfer@hotmail.fr>
2024-06-10 11:22:06 -0400

    Added Visual Studio Code

--- setup.sh
+++ setup.sh
@@ -115,6 +115,21 @@ apt install build-essential
 apt install cmake
 
 # .............................................................................
+# Visual Studio Code
+# .............................................................................
+
+# https://code.visualstudio.com/docs/setup/linux
+# https://github.com/erik1066/pop-os-setup/blob/master/README.md
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+# .............................................................................
 # Java
 # .............................................................................
 
